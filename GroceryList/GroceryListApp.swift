347b17f26310428ee3c891cb0fb16861e437bc95
Denis Chernyatin <ya.chernyatin@gmail.com>
2023-12-06 12:21:24 +0300

    Initial Commit

---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Denis Chernyatin on 2023.12.06.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
